d90268ec5ca47de0f77f9205c04f59b298bef7e2
Jazper <jazpersaldana@yahoo.com>
2024-05-24 19:11:24 -0500

    comp and gen lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_num = [n for n in num_list if n % 2 == 0]
+    return even_num
     pass
 
 def make_exclamation(sentence_list):
+    ex_list = [n + '!' for n in sentence_list]
+    return ex_list
     pass
